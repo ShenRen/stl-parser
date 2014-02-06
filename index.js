5600723e75e08aab7ba2d9c2c3685fe7643267c5
Caleb Duff <me@calebduff.com>
2014-02-06 17:57:37 -0600

    The code works now!
    Added some tests and finished up the code.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var cp = require('child_process')
-var parse = require('./parsestring.js')
 var concat = require('concat-stream')
 
 module.exports = function(admesh, file, callback){
-	var stlfile = { minx: 1, maxx: '', miny: '', maxy:'', minz: '', maxz: '', facets: '', volume: '' };
+	
 	var child = cp.spawn(admesh, [file])
 
 	var write = concat(function(code){
-		callback(false, stlfile)
+		parsedResult = {}
+		parsedResult.minx = parseFloat(/Min X = (.*),/.exec(code)[1])
+		parsedResult.minx = parseFloat(/Min X = (.*),/.exec(code)[1])
+		parsedResult.maxx = parseFloat(/Max X =\s*(.*)/.exec(code)[1])
+		parsedResult.miny = parseFloat(/Min Y = (.*),/.exec(code)[1])
+		parsedResult.maxy = parseFloat(/Max Y =\s*(.*)/.exec(code)[1])
+		parsedResult.minz = parseFloat(/Min Z = (.*),/.exec(code)[1])
+		parsedResult.maxz = parseFloat(/Max Z =\s*(.*)/.exec(code)[1])
+		parsedResult.facets = parseFloat(/Number of facets\s*:\s(\d*)/.exec(code)[1])
+		parsedResult.volume = parseFloat(/Volume\s*:\s*(.*)/.exec(code)[1])
+		callback(false, parsedResult)
 	})
 	child.stdout.pipe(write)
+	
 
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,56 @@
 var test = require('tap').test
 var index = require('../index.js')
-
 var admesh = '/Users/mac/Downloads/admesh-0.96/admesh'
 var file = '/Users/mac/code/stl-parser/gear.stl'
+
+var input = "======= Results produced by ADMesh version 0.96 ======\n"
+	+ "Input file         : /Users/mac/code/stl-parser/gear.stl\n"
+	+ "File type          : Binary STL file\n"
+	+ "Header             : STLB ASM 217.00.00.5800\n"
+	+ "============== Size ==============\n"
+	+ "Min X = -60.595921, Max X =  47.661232\n"
+	+ "Min Y = -47.652676, Max Y =  47.662365\n"
+	+ "Min Z = -4.000000, Max Z =  10.000000\n"
+	+ "========= Facet Status ========== Original ============ Final ==\n"
+	+ "Number of facets                 : 80146          80146\n"
+	+ "Facets with 1 disconnected edge  :     0              0\n"
+	+ "Facets with 2 disconnected edges :     0              0\n"
+	+ "Facets with 3 disconnected edges :     0              0\n"
+	+ "Total disconnected facets        :     0              0\n"
+	+ "=== Processing Statistics ===     ===== Other Statistics =====\n"
+	+ "Number of parts       :     1        Volume   :  31109.189453\n"
+	+ "Degenerate facets     :     0\n"
+	+ "Edges fixed           :     0\n"
+	+ "Facets removed        :     0\n"
+	+ "Facets added          :     0\n"
+	+ "Facets reversed       :     0\n"
+	+ "Backwards edges       :     0\n"
+	+ "Normals fixed         :    48\n"
+
+
+test('values are correct', function corectValues(t){
+	index(admesh, file, function(error, output){
+	t.equal(output.facets, 80146)
+	t.equal(output.minx, -60.595921)
+	t.equal(output.maxx, 47.661232)
+	t.equal(output.miny, -47.652676)
+	t.equal(output.maxy, 47.662365)
+	t.equal(output.maxz, 10.000000)
+	t.equal(output.minz, -4.000000)
+	t.equal(output.volume, 31109.189453)
+	t.end()	
+	})
+})
+
+test('theyre all numbers', function allNumbers(t){
+	index(admesh, file, function(error, output){
+		Object.getOwnPropertyNames(output).forEach(function(element, index, array){
+		t.equal(typeof output[element], 'number')
+		})
+		t.end()
+	})
+})
+
 
 test('only called once', function once(t){
 	t.plan(1)
@@ -27,9 +75,9 @@ test('its an object', function isIt(t){
 	})
 })
 
-test('the object has numbers', function numbers(t){
-	index(admesh, file, function(error, output){
-		t.equal(typeof output.minx, 'number')
-		t.end()
+test('testing a different file', function(t){
+	index(admesh, '/Users/mac/code/stl-parser/bridge.stl', function(error, output){
+		console.log(output)
 	})
+	t.end()
 })
